84b72440a3ffbbbf8e456f7f8c376d907a3bde72
yakovenko79 <imresquer@gmail.com>
2025-03-30 14:46:25 +0300

    6.1 Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,18 @@
+def test_user_login():
+    print('Hello!')
+
+
+class TestUserLogin:
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5, "wrong result"
