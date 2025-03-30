218e3dba4b082f043dff4ffdc60f7123606d8bae
yakovenko79 <imresquer@gmail.com>
2025-03-31 01:31:18 +0300

    6.5 Фикстуры Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -9,8 +9,6 @@ def test_empty_courses_list(chromium_page_with_state):
     expect(courses_main_header).to_be_visible()
     expect(courses_main_header).to_have_text('Courses')
 
-    chromium_page_with_state.wait_for_timeout(5000)
-
     results_header = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
     expect(results_header).to_be_visible()
     expect(results_header).to_have_text('There is no results')
