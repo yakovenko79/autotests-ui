263c7d9496684fb5298e7f443273e1e061ccfd95
yakovenko79 <imresquer@gmail.com>
2025-03-30 13:17:29 +0300

    5.6 Работа с авторизацией в Playwright

---
+++ playwright_courses.py
@@ -0,0 +1,36 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email_reg_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_reg_input.fill('user@gmail.com')
+
+    username_reg_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_reg_input.fill('username')
+
+    password_reg_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_reg_input.fill('password')
+
+    page.get_by_test_id('registration-page-registration-button').click()
+
+    context.storage_state(path='browser-state.json')
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False, )
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+    courses_header = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_header).to_be_visible()
+    expect(courses_header).to_have_text('Courses')
+
+    results = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(results).to_be_visible()
+    expect(results).to_have_text('There is no results')
