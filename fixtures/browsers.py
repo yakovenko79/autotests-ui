d18810e8de6a2ea9ca47916905bbd9646b1969fe
yakovenko79 <imresquer@gmail.com>
2025-03-31 22:08:12 +0300

    6.6 Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,10 +1,10 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page, Playwright, expect
+from playwright.sync_api import Page, Playwright
 
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
-    browser = playwright.firefox.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
     browser.close()
 
@@ -30,14 +30,12 @@ def initialize_browser_state(playwright: Playwright):
 
     context.storage_state(path='browser-state.json')
 
-    yield context
-
-    context.close()
     browser.close()
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state):
-    page = initialize_browser_state.new_page()
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-    yield page
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    yield context.new_page()
+    browser.close()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,10 +1,12 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 import pytest
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
     courses_main_header = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
     expect(courses_main_header).to_be_visible()
     expect(courses_main_header).to_have_text('Courses')
