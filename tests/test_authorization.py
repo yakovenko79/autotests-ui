f005392d195176377f5b6c91f5af22aa5d6c05e1
yakovenko79 <imresquer@gmail.com>
2025-04-02 23:12:34 +0300

    7.1 Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,6 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
 import pytest
+from  pages.login_page import LoginPage
 
 
 @pytest.mark.regression
@@ -12,22 +13,8 @@ import pytest
         ("  ", "password")
     ]
 )
-def test_wrong_email_or_password_authorization(email: str, password: str):
-    with sync_playwright() as playwright:
-        browser = playwright.firefox.launch(headless=False)
-        page = browser.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-        email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        email_input.fill(email)
-
-        password_input = page.get_by_test_id('login-form-password-input').locator('input')
-        password_input.fill(password)
-
-        page.get_by_test_id('login-page-login-button').click()
-
-        alert = page.get_by_role("alert")
-
-        expect(alert).to_be_visible()
-        expect(alert).to_have_text('Wrong email or password')
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
