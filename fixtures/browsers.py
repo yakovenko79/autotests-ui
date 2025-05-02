ddb93236a1eb24d5decf220d259c2f8bc7512dc4
yakovenko79 <imresquer@gmail.com>
2025-05-02 14:11:40 +0300

    10.4 Запуск автотестов на CI

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -16,7 +16,7 @@ def page(request: SubRequest, playwright: Playwright) -> Page:
         browser_type=request.param)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session ')
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url())
